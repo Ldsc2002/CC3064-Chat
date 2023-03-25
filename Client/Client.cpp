0bbf3036a348742b2d64385f7d6fbf70ad2e017c
Ldsc2002 <luissantoscuellar@gmail.com>
2023-03-24 20:40:33 -0600

    Getting client ip address

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -6,6 +6,47 @@
 #include <sys/socket.h>
 #include <cstring>
 
+using std::string;
+
+string getIP() {
+    const char* dnsServer = "8.8.8.8";
+    int dnsPort = 53;
+    struct sockaddr_in serv;
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(sock < 0) {
+        close(sock);
+        return "";
+    }
+
+    memset(&serv, 0, sizeof(serv));
+    serv.sin_family = AF_INET;
+    serv.sin_addr.s_addr = inet_addr(dnsServer);
+    serv.sin_port = htons(dnsPort);
+
+    int err = connect(sock, (const struct sockaddr*)&serv, sizeof(serv));
+    if (err < 0) {
+        close(sock);
+        return "";
+    }
+
+    struct sockaddr_in name;
+    socklen_t namelen = sizeof(name);
+    err = getsockname(sock, (struct sockaddr*)&name, &namelen);
+
+    char buffer[80];
+    const char* p = inet_ntop(AF_INET, &name.sin_addr, buffer, 80);
+    
+    if(p != NULL) {
+        close(sock);
+        string res = (string)buffer;
+        return res;
+    } else {
+        close(sock);
+        return "";
+    }
+}
+
 int main() {
     struct sockaddr_in serverAddress;
     int serverSocket;
@@ -30,6 +71,15 @@ int main() {
         printf("Error connecting to server\n");
         return 1;
     }
+
+    char buffer[1024] = {0};
+    printf("Enter enter email address: ");
+    scanf("%s", buffer);
+
+    string ip = getIP();
+
+    printf("Client IP: %s\n", ip.c_str());
+    printf("Email: %s\n", buffer);
 
     while (true) {
         char buffer[1024] = {0};
